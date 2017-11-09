e9cc512cb3b48bb3dd2003a90da980e53c69b85c
github username <12ivazquez@gmail.com>
2017-11-09 13:50:15 -0600

    add front-end framework + fetch API

---
+++ src/imgur.js
@@ -0,0 +1,10 @@
+import fetch from 'isomorphic-fetch';
+
+const apiURL = "https://api.imgur.com/3";
+const CLIENT_ID = CLIENT_ID;
+
+export function fetchImages() {
+  return(dispatch) => {
+    
+  }
+}
