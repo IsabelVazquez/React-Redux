dc6d88b8e4c79158aadc7c41935822112a77b079
github username <12ivazquez@gmail.com>
2017-11-19 12:55:50 -0600

    visible like button to logged users and successful POST image request

--- src/actions/image.js
+++ src/actions/image.js
@@ -1,26 +1,5 @@
 import ImageApi from './imageAPI';
 
 export function likeImage(credentials) {
-  return function(dispatch) {
-    return ImageApi.createImage(credentials)
-      .then(response => {
-        if(response) {
-          dispatch({
-            type: 'POST_IMAGE',
-            // response = {saved_image: true}
-            payload: response,
-          })
-        }
-        else {
-          dispatch({
-            type: 'FAIL_POST_IMAGE',
-            // response = {saved_image: false}
-            payload: response,
-          })
-        }
-      })
-      .catch(error => {
-        throw(error);
-      })
-  }
+  return ImageApi.createImage(credentials)
 }
--- src/components/Image.js
+++ src/components/Image.js
@@ -3,15 +3,24 @@ import { Col, Button } from 'react-bootstrap';
 import { likeImage } from '../actions/image';
 
 const Image = (props) => {
-  function handleClick(e) {
-    e.preventDefault();
-    likeImage(props.image);
+  var obj = JSON.parse(localStorage.getItem('user'));
+  var likeButton;
+  if (obj && obj.hasOwnProperty('name')) {
+    likeButton = <Button id={props.image.id} class="favorite" bsSize="xsmall" onClick={handleClick}><span class="glyphicon glyphicon-heart"></span></Button>
+  }
+  
+  function handleClick(ev) {
+    ev.preventDefault();
+    likeImage({
+      imgur_id: props.image.id,
+      user_id: obj.id
+    });
   }
 
   return (
     <Col md={2}>
       <div class="post">
-        <Button id={props.image.id} class="favorite" bsSize="xsmall" onClick={handleClick}><span class="glyphicon glyphicon-heart"></span></Button>
+        {likeButton}
         <a class="image-list-link" title={props.image.title} href={props.image.link} target="_blank" rel="noopener noreferrer">
           <img alt={"Click to view"} src={'http://imgur.com/' + props.image.cover + 't' + '.jpg'} />
         </a>
